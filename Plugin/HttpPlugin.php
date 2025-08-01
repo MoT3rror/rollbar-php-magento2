77372a0b8be6539d0ea9526225aee5e15227bbb0
Joshua Bixler <jbixler@vervocity.io>
2025-08-01 09:43:27 -0500

    Add typecasting to HttpPlugin

--- Plugin/HttpPlugin.php
+++ Plugin/HttpPlugin.php
@@ -2,9 +2,16 @@
 
 namespace Rollbar\Magento2\Plugin;
 
+use Exception;
+use Magento\Framework\App\Bootstrap;
+use Magento\Framework\App\Http;
+
 class HttpPlugin
 {
-	public function aroundCatchException($subject, $callable, $bootstrap, $exception): bool
+	/**
+	 * @param callable $callable
+	 */
+	public function aroundCatchException(Http $subject, $callable, Bootstrap $bootstrap, Exception $exception): bool
 	{
 		$callable($bootstrap, $exception);
 
